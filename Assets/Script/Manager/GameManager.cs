29668cb2d26e8987e7fb820ee1a3b1ef6b278e50
Scipion <leport.elouan@laposte.net>
2025-03-31 22:05:37 +0200

    Added Managers

---
+++ Assets/Script/Manager/GameManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+   
+    public static GameManager instance;
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+   
+}
---
+++ Assets/Script/Manager/LevelManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    
+    public static LevelManager instance;
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+}
