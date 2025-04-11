cc34d408fb2209caa0d7d7590999fcbd99296387
Scipion <leport.elouan@laposte.net>
2025-04-11 11:15:33 +0200

    Added Some Assets

---
+++ Assets/Packages/VV TexturePack/Script/AutoRotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+namespace SceneHelper {
+	public class AutoRotate: MonoBehaviour {
+	 
+	 public float xSpeed = 15.0f;
+	 public float ySpeed = 10.0f;
+	 public float zSpeed = 5.0f;
+	  
+	   // Update is called once per frame
+	   void Update () {
+	      // Rotation on x, y and z axis with different speed values 
+	      transform.Rotate( xSpeed * Time.deltaTime, ySpeed * Time.deltaTime, zSpeed * Time.deltaTime);
+	   }
+	}
+}
